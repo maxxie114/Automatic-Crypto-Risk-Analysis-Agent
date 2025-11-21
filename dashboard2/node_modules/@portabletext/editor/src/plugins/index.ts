dbe1018b0945a2d304a1ba7f4b76945e6c4557ba
Max Xie <maxxie114@gmail.com>
2025-11-21 12:34:48 -0800

    remove node modules

--- dashboard2/node_modules/@portabletext/editor/src/plugins/index.ts
+++
@@ -1,3 +0,0 @@
-export {BehaviorPlugin} from './plugin.behavior'
-export {EditorRefPlugin} from './plugin.editor-ref'
-export {EventListenerPlugin} from './plugin.event-listener'
--- dashboard2/node_modules/@portabletext/editor/src/selectors/selector.is-selection-expanded.ts
+++
@@ -1,9 +0,0 @@
-import type {EditorSelector} from '../editor/editor-selector'
-import {isSelectionCollapsed} from './selector.is-selection-collapsed'
-
-/**
- * @public
- */
-export const isSelectionExpanded: EditorSelector<boolean> = (snapshot) => {
-  return snapshot.context.selection !== null && !isSelectionCollapsed(snapshot)
-}
